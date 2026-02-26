18c9816b48d12e13cf2bab7262d8ae4aa553fc3c
Manav Thakur <manavthakur652@gmail.com>
2026-02-27 02:45:59 +0530

    Fix incorrect relative path to user model"

--- Backend/src/lib/inngest.js
+++ Backend/src/lib/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest"
 import { connectDB } from "./db.js"
-import User from "../../src/models/user.js"
+import User from "../../models/user.js"
 
 export const inngest = new Inngest({ id: "interviewer" })
 
