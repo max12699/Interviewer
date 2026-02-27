4d8a394b30bf10b3761442ae1a37ac40926f762b
Manav Thakur <manavthakur652@gmail.com>
2026-02-27 16:57:31 +0530

    again modify

--- Backend/src/lib/inngest.js
+++ Backend/src/lib/inngest.js
@@ -1,7 +1,7 @@
 
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
-import User from "../models/User.js";
+import User from "../../src/models/User.js";
 
 
 export const inngest = new Inngest({ id: "talent-iq" });
