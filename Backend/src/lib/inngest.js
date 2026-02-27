75f9b373a6198f2a644a847b8bbf37e46eeea340
Manav Thakur <manavthakur652@gmail.com>
2026-02-27 14:57:04 +0530

    Fix import path for User model and improve formatting in user schema

--- Backend/src/lib/inngest.js
+++ Backend/src/lib/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest"
 import { connectDB } from "./db.js"
-import User from "../../src/models/user"
+import User from "../../src/models/User.js"
 
 export const inngest = new Inngest({ id: "interviewer" })
 
