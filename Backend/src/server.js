2c9aeba69ab2af7b23a790d2cf4a37601a13f4ba
Manav Thakur <manavthakur652@gmail.com>
2026-02-27 15:51:00 +0530

    "Fix express middleware and inngest setup"

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,18 +5,21 @@ import { fileURLToPath } from "url"
 import { ENV } from "./lib/env.js"
 import { connectDB } from "./lib/db.js"
 import { serve } from "inngest/express"
-import { inngest } from "./lib/inngest.js"
+import { inngest, functions } from "./lib/inngest.js"
 
 const app = express()
 
-// middleware
-app.use(express.join())
-//Credentials : true meanning?? => server allow a broswer to include cookies on req
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// CORS
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }))
 
+// Inngest Route
 app.use("/api/inngest", serve({ client: inngest, functions }))
 
-//  __dirname for ES modules
+// ES Module __dirname fix
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
@@ -36,19 +39,20 @@ if (ENV.NODE_ENV === "production") {
 
   app.use(express.static(frontendPath))
 
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"))
   })
 }
 
 const startServer = async () => {
   try {
-    app.listen(ENV.PORT, async () => {
-      await connectDB()
+    await connectDB()
+
+    app.listen(ENV.PORT, () => {
       console.log(`Server is running on port: ${ENV.PORT}`)
     })
   } catch (error) {
-    console.error("error starting the server ", error)
+    console.error("Error starting server:", error)
   }
 }
 
