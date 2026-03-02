d33d300acec2d27c443340e982b12cbd25419778
Manav Thakur <manavthakur652@gmail.com>
2026-03-02 13:39:57 +0530

    fix: correct import path for sessionRoutes and comment out unused CORS options

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -9,7 +9,7 @@ import { connectDB } from "./lib/db.js";
 import { inngest, functions } from "./lib/inngest.js";
 
 import chatRoutes from "./routes/chatRoutes.js";
-import sessionRoutes from "./routes/sessionRoute.js";
+import sessionRoutes from "./routes/sessionRoutes.js";
 
 const app = express();
 
@@ -25,7 +25,7 @@ app.use(
   })
 );
 
-app.options("*", cors());
+//app.options("*", cors());
 app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
